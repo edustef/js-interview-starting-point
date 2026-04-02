77e6976398ecac098f342c13766eb31a7f5296aa
edustef <eduardstefan290@gmail.com>
2026-04-02 14:16:33 +0300

    feat: get token programatically and remove .env

--- src/env.js
+++
@@ -1,5 +0,0 @@
-if (!process.env.API_TOKEN) {
-		throw new Error('API_TOKEN is not defined in environment variables');
-	}
-
-export const apiToken = process.env.API_TOKEN;
--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,38 @@
 import { z } from 'zod';
-import { apiToken } from './env.js';
 
 export const positionSchema = z.object({
   x: z.coerce.number({ message: 'x must be a number' }),
   y: z.coerce.number({ message: 'y must be a number' }),
 });
 
-export const shopsSchema = z.array(positionSchema);
+export const shopsSchema = z.array(positionSchema.extend({
+  id: z.number(),
+  name: z.string(),
+  created_at: z.iso.datetime(),
+  updated_at: z.iso.datetime(),
+}));
 
 export async function fetchShops() {
   const url = 'https://api-challenge.agilefreaks.com/v1/coffee_shops';
-  const urlWithToken = `${url}?token=${(apiToken)}`;
+  const apiToken = await getToken();
+  const urlWithToken = `${url}?token=${apiToken}`;
 
   const response = await fetch(urlWithToken, {
-    headers: { 'Accept': 'application/json'  },
+    headers: { 'Accept': 'application/json' },
+  });
+
+  const data = await response.json();
+  return shopsSchema.parse(data);
+}
+
+/**
+ *  @returns {Promise<string>} The API token as a string.
+ */
+export async function getToken() {
+  const url = 'https://api-challenge.agilefreaks.com/v1/tokens';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Accept': 'application/json' },
   });
 
   if (!response.ok) {
@@ -21,5 +40,5 @@ export async function fetchShops() {
   }
 
   const data = await response.json();
-  return shopsSchema.parse(data);
+  return data.token;
 }
